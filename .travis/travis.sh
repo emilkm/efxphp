4909f782a161817a4fb79f0f2a16c3d534088714
emilkm <emil@localhost>
2017-02-07 09:27:59 +1300

    Update travis script.

--- .travis/travis.sh
+++ .travis/travis.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-EFXPHP_PATH=$(pwd)
-echo $EFXPHP_PATH
 
 # PHP Settings
 PHP_NAME="php-$PHP_VERSION"
 PHP_PACKAGE="php-$PHP_VERSION.tar.gz"
 PHP_URL="http://us1.php.net/get/$PHP_PACKAGE/from/this/mirror"
-PHP_CLI="/home/travis/build/emilkm/efxphp/php-7.1.1/install/bin/php"
+
 
 # Move out of project
+cd ..
 BASE_PATH=$(pwd)
+PHP_CLI="$BASE_PATH/$PHP_NAME/install/bin/php"
 
 
-if [ -e "/home/travis/build/emilkm/efxphp/php-7.1.1/configure" ] 
+if [ -e "$BASE_PATH/$PHP_NAME/configure" ] 
 then
-	echo "File /home/travis/build/emilkm/efxphp/php-7.1.1/configure exists." 
+	echo "File BASE_PATH/$PHP_NAME/configure exists." 
 else
     # Get and extract prebuilt PHP
 	wget "$PHP_URL" -O $PHP_PACKAGE
@@ -24,12 +24,12 @@ else
 	# Build PHP
 	cd $PHP_NAME
 	./buildconf --force
-	./configure --enable-debug --disable-all --enable-libxml --enable-simplexml --enable-dom --enable-phar --prefix=/home/travis/build/emilkm/efxphp/php-7.1.1/install
+	./configure --enable-debug --disable-all --enable-libxml --enable-simplexml --enable-dom --enable-phar --prefix=$BASE_PATH/$PHP_NAME/install
 	make install
 fi
 
 # Get AMFEXT
-cd $EFXPHP_PATH/$PHP_NAME/ext
+cd $BASE_PATH/$PHP_NAME/ext
 rm -fr ./amf
 git clone --depth 1 https://github.com/emilkm/amfext.git amf
 
@@ -37,16 +37,15 @@ git clone --depth 1 https://github.com/emilkm/amfext.git amf
 # Build AMFEXT
 cd $PHP_NAME/ext/amf
 ../../install/bin/phpize
-./configure --with-php-config=/home/travis/build/emilkm/efxphp/php-7.1.1/install/bin/php-config
+./configure --with-php-config=$BASE_PATH/$PHP_NAME/install/bin/php-config
 make install
-rm -fr $EFXPHP_PATH/$PHP_NAME/ext/amf
+rm -fr $BASE_PATH/$PHP_NAME/ext/amf
 
 # Run tests
 export REPORT_EXIT_STATUS=1
 export NO_INTERACTION=1
 
-cd $EFXPHP_PATH/tests
-echo "CWD="$(pwd)
+cd $TRAVIS_BUILD_DIR/tests
 wget https://phar.phpunit.de/phpunit.phar -O phpunit.phar
 $PHP_CLI -dextension=amf.so -m
 $PHP_CLI -dextension=amf.so phpunit.phar --configuration phpunit.xml
