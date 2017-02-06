181db6b6e10c55a7e6b87c100f387c06a7665765
emilkm <emilkm@users.noreply.github.com>
2017-02-06 17:50:16 +1300

    Try to speed up the build with a pre-built php.

--- .travis/travis.sh
+++ .travis/travis.sh
@@ -4,15 +4,16 @@ EFXPHP_PATH=$(pwd)
 
 # PHP Settings
 PHP_NAME="php-$PHP_VERSION"
-PHP_PACKAGE="php-$PHP_VERSION.tar.gz"
-PHP_URL="http://us1.php.net/get/$PHP_PACKAGE/from/this/mirror"
+PHP_PACKAGE="php-$PHP_VERSION-travis.tar.gz"
+PHP_URL="http://www.emilmalinov.com/travis/$PHP_PACKAGE"
+PHP_CLI="/home/travis/build/emilkm/php-7.1.1/install/bin/php"
 
 # Move out of project 
 cd ../
 BASE_PATH=$(pwd)
 
-# Get and extract PHP
-wget $PHP_URL -O $PHP_PACKAGE
+# Get and extract prebuilt PHP
+wget "$PHP_URL" -O $PHP_PACKAGE
 tar -xf $PHP_PACKAGE 
 
 # Get AMFEXT
@@ -20,17 +21,20 @@ git clone --depth 1 https://github.com/emilkm/amfext.git amfext
 mv amfext $PHP_NAME/ext/amf
 
 
-# Build PHP
-cd $PHP_NAME
-./buildconf --force
-./configure --enable-debug --disable-all --enable-libxml --enable-simplexml --enable-dom --with-amf
-make
+# Build AMFEXT
+cd $PHP_NAME/ext/amf
+../../install/bin/phpize
+./configure --with-php-config=/home/travis/build/emilkm/php-7.1.1/install/bin/php-config
+make install
+
+# Get PHPUNIT
+wget -O https://phar.phpunit.de/phpunit.phar
 
 # Run tests
 export REPORT_EXIT_STATUS=1
-export TEST_PHP_EXECUTABLE=sapi/cli/php
 export NO_INTERACTION=1
-sapi/cli/php run-tests.php ext/amf  
 
 cd $EFXPHP_PATH
-$BASE_PATH/$PHP_NAME/sapi/cli/php phpunit --configuration test/phpunit.xml
+$PHP_CLI -dextension=amf.so -m
+$PHP_CLI -dextension=amf.so phpunit --configuration test/phpunit.xml
+
