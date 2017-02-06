ab95c11e070761996988b6dc52f81d18dfd8eb07
emilkm <emil@localhost>
2017-02-07 12:22:06 +1300

    Update tests/bootstrap.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,10 +8,28 @@ $autoLoader = new ClassLoader();
 $autoLoader->setUseIncludePath(true);
 $autoLoader->addPsr4('emilkm\\efxphp\\', BASE . 'src/');
 $autoLoader->addPsr4('emilkm\\tests\\', BASE . 'tests/');
+
+if (file_exists(BASE . 'vendor/autoload.php')) {
+    $map = require BASE . 'vendor/composer/autoload_namespaces.php';
+    foreach ($map as $namespace => $path) {
+        $autoLoader->set($namespace, $path);
+    }
+
+    $map = require  BASE . 'vendor/composer/autoload_psr4.php';
+    foreach ($map as $namespace => $path) {
+        $autoLoader->setPsr4($namespace, $path);
+    }
+
+    $classMap = require  BASE . 'vendor/composer/autoload_classmap.php';
+    if ($classMap) {
+        $autoLoader->addClassMap($classMap);
+    }
+}
+
 $autoLoader->register(true);
 
-require_once BASE . 'vendor/autoload.php';
 
-use emilkm\tests\util\PhpServer;
-$phpServer = new PhpServer('127.0.0.1', 9999, BASE . 'tests/webapp/public');
-$pid = $phpServer->start();
+
+//use emilkm\tests\util\PhpServer;
+//$phpServer = new PhpServer('127.0.0.1', 9999, BASE . 'tests/webapp/public');
+//$pid = $phpServer->start();
